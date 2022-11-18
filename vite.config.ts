a6be514880a75df1cfc17b23b371aa13459b9547
Mike Dilger <mike@mikedilger.com>
2022-11-19 11:35:50 +1300

    Configured vite for use with tauri

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,23 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+    plugins: [vue()],
+    // prevent vite from obscuring rust errors
+    clearScreen: false,
+    // Tauri expects a fixed port, fail if that port is not available
+    server: {
+        strictPort: true,
+    },
+    // to make use of `TAURI_PLATFORM`, `TAURI_ARCH`, `TAURI_FAMILY`,
+    // `TAURI_PLATFORM_VERSION`, `TAURI_PLATFORM_TYPE` and `TAURI_DEBUG`
+    // env variables
+    envPrefix: ['VITE_', 'TAURI_'],
+    build: {
+        // Tauri supports es2021
+        target: ['es2021', 'chrome100', 'safari13'],
+        // don't minify for debug builds
+        minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+        // produce sourcemaps for debug builds
+        sourcemap: !!process.env.TAURI_DEBUG,
+    },
 })
