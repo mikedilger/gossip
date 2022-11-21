5af1544383ed7401f4c8a47b6c6ed3ef76fb6884
Mike Dilger <mike@mikedilger.com>
2022-11-21 19:54:53 +1300

    DateAgo updated to keep ticking over the time

--- src/components/DateAgo.vue
+++ src/components/DateAgo.vue
@@ -1,9 +1,10 @@
 <script setup>
-    import { computed } from 'vue'
+    import { reactive, onMounted } from 'vue'
 
     const props = defineProps(['date'])
+    const state = reactive({ ago: "", timeout: 500 })
 
-    const ago = computed(() => {
+    function updateAgo() {
         let now = Date.now() / 1000;
         let seconds = now - props.date;
         let minutes = seconds / 60;
@@ -11,30 +12,56 @@
         let days = hours / 24;
         let years = days / 365;
 
-        if (seconds < 45) return Math.round(seconds) + "s";
-        if (seconds < 90) return "1m";
-        if (minutes < 45) return Math.round(minutes) + "m";
-        if (minutes < 90) return "1h";
-        if (hours < 24) return Math.round(hours) + "h";
-        if (hours < 42) return "1d";
-        if (days < 30) return Math.round(days) + "d";
-        if (days < 45) return "1m";
-        if (days < 365) return Math.round(days / 30) + "m";
-        if (years < 1.5) return "1y";
-        return Math.round(years) + "y";
-    })
+        if (seconds < 45) {
+            state.ago = Math.round(seconds) + "s";
+            state.timeout = 500;
+        } else if (seconds < 90) {
+            state.ago = "1m";
+            state.timeout = 1000;
+        }
+        else if (minutes < 45) {
+            state.ago = Math.round(minutes) + "m";
+            state.timeout = 500 * 60;
+        } else if (minutes < 90) {
+            state.ago = "1h";
+            state.timeout = 1000 * 60;
+        } else if (hours < 24) {
+            state.ago = Math.round(hours) + "h";
+            state.timeout = 500 * 60 * 60;
+        } else if (hours < 42) {
+            state.ago = "1d";
+            state.timeout = 1000 * 60 * 60;
+        } else if (days < 30) {
+            state.ago = Math.round(days) + "d";
+            state.timeout = 500 * 60 * 60 * 24;
+        } else if (days < 45) {
+            state.ago = "1m";
+            state.timeout = 1000 * 60 * 60 * 24;
+        } else if (days < 365) {
+            state.ago = Math.round(days / 30) + "m";
+            state.timeout = 500 * 60 * 60 * 24 * 30;
+        } else if (years < 1.5) {
+            state.ago = "1y";
+            state.timeout = 1000 * 60 * 60 * 24 * 30;
+        } else {
+            state.ago = Math.round(years) + "y";
+            state.timeout = null;
+        }
+    }
 
+    onMounted(() => {
+        updateAgo();
+        setInterval(() => updateAgo(), state.timeout)
+    })
 </script>
 
 <template>
-    <span class="dateago">
-        {{ ago }}
-    </span>
+    <span class="dateago">{{ state.ago }}</span>
 </template>
 
 <style scoped>
     span.dateago {
-        color: #b1a296;
-        padding-left: 2em;
+        display: inline-block;
+        width: 4em;
     }
 </style>
