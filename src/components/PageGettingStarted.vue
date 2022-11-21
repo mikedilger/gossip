eb1b7238229c97f5c37c5f256c180fd7651924d8
Mike Dilger <mike@mikedilger.com>
2022-11-22 06:14:41 +1300

    Getting started page - probably should be a component on Feed page.

---
+++ src/components/PageGettingStarted.vue
@@ -0,0 +1,44 @@
+<script setup>
+</script>
+
+<template>
+    <h2>Getting Started</h2>
+    <div class="main-scrollable">
+        <h3>Welcome to Gossip</h3>
+
+        <p>
+            In order to follow someone, you need to know their public key and also which relay(s) they post to. Go to the <a href="/subscriptions">Subscriptions</a> tab to follow people.
+        </p>
+
+        <p>
+            In order to post messages yourself, you need to have an identity. Go to the <a href="/identities">Identities</a> tab to create one.
+        </p>
+
+        <hr>
+
+        <p>
+            Gossip is a nostr client.
+        </p>
+
+        <p>
+            Nostr is a protocol and specification for storing and retrieving social media events onto servers called relays. Many users store their events onto multiple relays for reliability, censorship resistance, and to spread their reach. If you didn't store an event on a particular relay, don't expect anyone to find it there because relays normally don't share events with each other.
+        </p>
+
+        <p>
+            Users are defined by their keypair, and are known by the public key of that pair. All events they generate are signed by their private key, and verifiable by their public key.
+        </p>
+
+        <p>
+            In order to follow someone, you need to know their public key and also which relay(s) they post to.
+        </p>
+    </div>
+</template>
+
+<style scoped>
+    div.main-scrollable {
+        margin-top: 1em;
+        padding-right: max(2em, 6vw);
+        max-height: calc(100vh - 41px);
+        overflow-y: scroll;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue'
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 import PageFeed from './components/PageFeed.vue'
+import PageGettingStarted from './components/PageGettingStarted.vue'
 import PageSubscriptions from './components/PageSubscriptions.vue'
 import PageIdentities from './components/PageIdentities.vue'
 import PageSettings from './components/PageSettings.vue'
@@ -11,6 +12,7 @@ import PageAbout from './components/PageAbout.vue'
 
 const routes = [
     { path: '/', component: PageFeed },
+    { path: '/getting-started', component: PageGettingStarted },
     { path: '/subscriptions', component: PageSubscriptions },
     { path: '/identities', component: PageIdentities },
     { path: '/settings', component: PageSettings },
