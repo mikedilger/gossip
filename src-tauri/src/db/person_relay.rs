34a95996cc0a19257710bb95a711a698a9b8bfa4
Mike Dilger <mike@mikedilger.com>
2022-11-25 20:45:52 +1300

    DbPersonRelay:fetch_for_pubkeys()

--- src-tauri/src/db/person_relay.rs
+++ src-tauri/src/db/person_relay.rs
@@ -1,5 +1,6 @@
 use crate::{Error, GLOBALS};
 use serde::{Deserialize, Serialize};
+use nostr_proto::PublicKeyHex;
 use tauri::async_runtime::spawn_blocking;
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -44,6 +45,49 @@ impl DbPersonRelay {
         output
     }
 
+    /// Fetch records matching the given public keys, ordered from highest to lowest rank
+    #[allow(dead_code)]
+    pub async fn fetch_for_pubkeys(pubkeys: &[PublicKeyHex])
+                                   -> Result<Vec<DbPersonRelay>, Error>
+    {
+        let sql = format!(
+            "SELECT person, relay, recommended, person_relay.last_fetched \
+             FROM person_relay \
+             INNER JOIN relay ON person_relay.relay=relay.url \
+             WHERE person IN ({}) ORDER BY person, relay.rank DESC",
+            repeat_vars(pubkeys.len())
+        );
+
+        let pubkey_strings: Vec<String> = pubkeys.iter().map(|p| p.0.clone()).collect();
+
+        let output: Result<Vec<DbPersonRelay>, Error> = spawn_blocking(move || {
+            let maybe_db = GLOBALS.db.blocking_lock();
+            let db = maybe_db.as_ref().unwrap();
+
+            let mut stmt = db.prepare(&sql)?;
+            let rows = stmt.query_map(
+                rusqlite::params_from_iter(pubkey_strings),
+                |row| {
+                    Ok(DbPersonRelay {
+                        person: row.get(0)?,
+                        relay: row.get(1)?,
+                        recommended: row.get(2)?,
+                        last_fetched: row.get(3)?,
+                    })
+                }
+            )?;
+
+            let mut output: Vec<DbPersonRelay> = Vec::new();
+            for row in rows {
+                output.push(row?);
+            }
+           Ok(output)
+        })
+        .await?;
+
+        output
+    }
+
     #[allow(dead_code)]
     pub async fn insert(person_relay: DbPersonRelay) -> Result<(), Error> {
         let sql =
@@ -80,5 +124,12 @@ impl DbPersonRelay {
 
         Ok(())
     }
+}
 
+fn repeat_vars(count: usize) -> String {
+    assert_ne!(count, 0);
+    let mut s = "?,".repeat(count);
+    // Remove trailing comma
+    s.pop();
+    s
 }
