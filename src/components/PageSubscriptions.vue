dba039ab24d42eaa8382859bf6be9f78cf778a86
Mike Dilger <mike@mikedilger.com>
2022-11-27 14:51:35 +1300

    People: List people followed

--- src/components/PageSubscriptions.vue
+++ src/components/PageSubscriptions.vue
@@ -1,6 +1,13 @@
 <script setup>
     import { reactive } from 'vue'
     import { invoke } from '@tauri-apps/api/tauri'
+    import { storeToRefs } from 'pinia'
+    import { useEventStore } from '../eventStore.js'
+    import Avatar from './Avatar.vue'
+    import Name from './Name.vue'
+    import Nip05 from './Nip05.vue'
+    import IconWalk from './IconWalk.vue'
+    import PubKey from './PubKey.vue'
 
     const state = reactive({
         tab: 'add_new',
@@ -10,6 +17,11 @@
         alert: null,
     });
 
+    const store = useEventStore();
+    const { people } = storeToRefs(store)
+    //    { pubkey, name, about, picture,
+    //      dns_id, dns_id_valid, dns_id_last_checked, followed }
+
     function follow_nip35() {
         invoke('follow_nip35', { address: state.address })
             .then((success) => statea.alert = "Client restart required")
@@ -17,7 +29,7 @@
     }
 
     function follow_key_and_relay() {
-        invoke('follow_pubkey_relay', { pubkey: pubkey, relay: relay })
+        invoke('follow_key_and_relay', { pubkey: state.pubkey, relay: state.relay })
             .then((success) => state.alert = "Client restart required")
             .catch((error) => state.alert = error)
     }
@@ -57,10 +69,10 @@
                     enter that here:
                 </p>
                 <span>This should look like <b>f6429d976e0724fa67f496393e3696f96908f985f054a3ffc717156fe004cae6</b></span><br>
-                Public Key: <input type="text" size="65" /><br>
+                Public Key: <input type="text" v-model="state.pubkey" size="65" /><br>
                 <br>
                 <span>This should look like <b>wss://nostr-relay.example.com</b></span><br>
-                Relay URL: <input type="text" size="40" /><br>
+                Relay URL: <input type="text" v-model="state.relay" size="40" /><br>
                 <br>
                 <div class="center follow-button">
                     <button @click="follow_key_and_relay()">Follow</button>
@@ -96,7 +108,20 @@
         </div>
         <div v-if="state.tab=='following'">
             <h2>Following</h2>
-            <p>TBD</p>
+            <p v-for="[pubkey,person] in people">
+                <div class="person-row">
+                    <div class="avatar-column">
+                        <Avatar :url="person.picture"></Avatar>
+                    </div>
+                    <div class="person-column">
+                        <PubKey :pubkey="person.pubkey"></PubKey>
+                        <br>
+                        <Name :name="person.name"></Name>
+                        <br>
+                        {{ person.about }}
+                    </div>
+                </div>
+            </p>
         </div>
         <div v-if="state.tab=='not_following'">
             <h2>Not Following</h2>
@@ -115,5 +140,20 @@
     div.alert {
         font-size: 2em;
         border: 1px solid black;
+    }
+    div.person-row {
+        display: flex;
+        border-bottom: 1px solid #505050;
+    }
+    div.avatar-column {
+        flex: 0;
+    }
+    div.person-column {
+        flex: 1;
+    }
+    @media (prefers-color-scheme: light) {
+        div.person-row {
+            border-bottom: 1px solid #e8e8e8;
+        }
     }
 </style>
