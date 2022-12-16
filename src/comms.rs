a78222bacdd2e9d978ea0de8e7d42b63c304f063
Mike Dilger <mike@mikedilger.com>
2022-12-17 07:55:35 +1300

    zeroize

--- src/comms.rs
+++ src/comms.rs
@@ -1,4 +1,7 @@
+use std::ops::Drop;
+
 use serde::Serialize;
+use zeroize::Zeroize;
 
 /// This is a message sent between the Overlord and Minions
 /// in either direction
@@ -12,4 +15,12 @@ pub struct BusMessage {
 
     /// The payload, serialized as a JSON string
     pub json_payload: String,
+}
+
+/// We may send passwords through BusMessage objects, so we zeroize
+/// bus message payloads upon drop.
+impl Drop for BusMessage {
+    fn drop(&mut self) {
+        self.json_payload.zeroize();
+    }
 }
