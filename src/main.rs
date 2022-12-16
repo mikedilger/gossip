ea3614c68f37d5714e0fa86ace4d7cb966a6eb0d
Mike Dilger <mike@mikedilger.com>
2022-12-17 07:28:59 +1300

    Logging

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,12 @@
-fn main() {}
+use std::env;
+
+fn main() {
+    // Set up logging
+    if env::var("RUST_LOG").is_err() {
+        env::set_var("RUST_LOG", "info");
+    }
+    if env::var("RUST_LOG_STYLE").is_err() {
+        env::set_var("RUST_LOG_STYLE", "auto");
+    }
+    env_logger::init();
+}
