bedd0af6a7d5ad29a2c03fd8ec7d42908a7867d7
Mike Dilger <mike@mikedilger.com>
2022-12-18 14:04:09 +1300

    Handle tokio related errors

--- src/error.rs
+++ src/error.rs
@@ -1,9 +1,25 @@
+use crate::comms::BusMessage;
 use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum Error {
+    #[error("Error broadcasting: {0}")]
+    BroadcastSend(#[from] tokio::sync::broadcast::error::SendError<BusMessage>),
+
+    #[error("Error receiving broadcast: {0}")]
+    BroadcastReceive(#[from] tokio::sync::broadcast::error::RecvError),
+
     #[error("Error: {0}")]
     General(String),
+
+    #[error("Task join error: {0}")]
+    JoinError(#[from] tokio::task::JoinError),
+
+    #[error("Error sending mpsc: {0}")]
+    MpscSend(#[from] tokio::sync::mpsc::error::SendError<BusMessage>),
+
+    #[error("Bad integer: {0}")]
+    ParseInt(#[from] std::num::ParseIntError),
 }
 
 impl From<String> for Error {
