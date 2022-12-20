fe541f9e6eb306ef8a6a4ee19a5fe5abbf76035f
Mike Dilger <mike@mikedilger.com>
2022-12-20 19:07:05 +1300

    Initial egui-eframe hello world application

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
 #[macro_use]
 extern crate lazy_static;
 
@@ -5,7 +7,15 @@ mod comms;
 mod db;
 mod error;
 mod globals;
+mod ui;
 
 fn main() {
     tracing_subscriber::fmt::init();
+
+    // TBD: start async code
+
+    ui::run();
+
+    // TBD: Tell the async parties to close down
+    // TBD: wait for the async parties to close down
 }
---
+++ src/ui/mod.rs
@@ -0,0 +1,47 @@
+
+use eframe::egui;
+
+pub fn run() {
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(320.0, 240.0)),
+        ..Default::default()
+    };
+
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|_cc| Box::new(MyApp::default())),
+    )
+}
+
+struct MyApp {
+    name: String,
+    age: u32,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self {
+            name: "Arthur".to_owned(),
+            age: 42,
+        }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("My egui Application");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Your name: ");
+                ui.text_edit_singleline(&mut self.name)
+                    .labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Click each year").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+        });
+    }
+}
