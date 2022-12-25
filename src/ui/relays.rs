90563d4da65088e2d2d0e2e65a2ef220cefb4a87
Mike Dilger <mike@mikedilger.com>
2022-12-26 09:50:48 +1300

    ui: relays page: sorted

--- src/ui/relays.rs
+++ src/ui/relays.rs
@@ -1,4 +1,5 @@
 use super::GossipUi;
+use crate::db::DbRelay;
 use crate::globals::GLOBALS;
 use eframe::egui;
 use egui::{Align, Context, Layout, ScrollArea, Ui};
@@ -8,10 +9,12 @@ pub(super) fn update(_app: &mut GossipUi, _ctx: &Context, _frame: &mut eframe::F
     ui.heading("Relays known");
     ui.add_space(18.0);
 
-    let relays = GLOBALS.relays.blocking_lock().clone();
+    let mut relays = GLOBALS.relays.blocking_lock().clone();
+    let mut relays: Vec<DbRelay> = relays.drain().map(|(_,relay)| relay).collect();
+    relays.sort_by(|a,b| a.url.cmp(&b.url));
 
     ScrollArea::vertical().show(ui, |ui| {
-        for (_, relay) in relays.iter() {
+        for relay in relays.iter() {
             ui.horizontal(|ui| {
                 ui.label(&relay.url);
 
