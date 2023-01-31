afc8da330f06ad878a2a3c1a4448caf2947b090d
Mike Dilger <mike@mikedilger.com>
2023-01-31 20:42:02 +1300

    SCHEMA 18: delete records with urls that start or end in whitespace

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -103,7 +103,7 @@ macro_rules! apply_sql {
 }
 
 fn upgrade(db: &Connection, mut version: u16) -> Result<(), Error> {
-    let current_version = 17;
+    let current_version = 18;
     if version > current_version {
         panic!(
             "Database version {} is newer than this binary which expects version {}.",
@@ -130,6 +130,7 @@ fn upgrade(db: &Connection, mut version: u16) -> Result<(), Error> {
     apply_sql!(db, version, 15, "schema15.sql");
     apply_sql!(db, version, 16, "schema16.sql");
     apply_sql!(db, version, 17, "schema17.sql");
+    apply_sql!(db, version, 18, "schema18.sql");
     tracing::info!("Database is at version {}", version);
     Ok(())
 }
