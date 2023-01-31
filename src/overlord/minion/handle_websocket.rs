9eb43a506470cc78d0d68605b5a13caf5a25823c
Mike Dilger <mike@ezicheq.com>
2023-02-01 11:18:10 +1300

    Fix excessive closing of websockets

--- src/overlord/minion/handle_websocket.rs
+++ src/overlord/minion/handle_websocket.rs
@@ -88,13 +88,16 @@ impl Minion {
                     .get_handle_by_id(&subid.0)
                     .unwrap_or_else(|| "_".to_owned());
 
-                let close: bool = handle.starts_with("temp_");
+                // If we opened a temporary subscription, and it is the only subscription on this
+                // relay, we will close it.
+                let close: bool = handle.starts_with("temp_") && self.subscriptions.len() <= 1;
 
                 // Update the matching subscription
                 match self.subscriptions.get_mut_by_id(&subid.0) {
                     Some(sub) => {
                         tracing::debug!("{}: {}: EOSE: {:?}", &self.url, handle, subid);
                         if close {
+                            tracing::debug!("{}: {}: Closing websocket", &self.url, handle);
                             let close_message = sub.close_message();
                             let websocket_sink = self.sink.as_mut().unwrap();
                             let wire = serde_json::to_string(&close_message)?;
