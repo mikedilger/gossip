6e5c2562d8301ff28d32656b1095bf0834be8a78
Mike Dilger <mike@mikedilger.com>
2023-02-04 18:59:41 +1300

    Improve the live relay page

--- src/ui/relays/mod.rs
+++ src/ui/relays/mod.rs
@@ -1,7 +1,9 @@
 use super::{GossipUi, Page};
+use crate::db::DbRelay;
 use crate::globals::GLOBALS;
+use crate::relay_assignment::RelayAssignment;
 use eframe::egui;
-use egui::{Align, Context, Layout, SelectableLabel, Ui};
+use egui::{Context, ScrollArea, SelectableLabel, Ui};
 use egui_extras::{Column, TableBuilder};
 
 mod all;
@@ -31,20 +33,24 @@ pub(super) fn update(app: &mut GossipUi, ctx: &Context, frame: &mut eframe::Fram
     if app.page == Page::RelaysLive {
         ui.add_space(10.0);
 
-        ui.with_layout(Layout::top_down(Align::Min), |ui| {
-            ui.heading("Connected To");
-            for url in GLOBALS.relays_watching.blocking_read().iter() {
-                ui.label(&url.0);
-            }
-        });
+        ui.heading("Connected Relays");
+        ui.add_space(18.0);
 
-        ui.add_space(10.0);
-        ui.separator();
-        ui.add_space(10.0);
-
-        ui.heading("Serving Long-term General Feed");
-
-        let relay_assignments = GLOBALS.relay_assignments.blocking_read().clone();
+        let relays_watching = GLOBALS.relays_watching.blocking_read().clone();
+        let mut relay_assignments = GLOBALS.relay_assignments.blocking_read().clone();
+        let relays: Vec<RelayAssignment> = relays_watching
+            .iter()
+            .map(|url| {
+                if let Some(pos) = relay_assignments.iter().position(|r| r.relay.url == *url) {
+                    relay_assignments.swap_remove(pos)
+                } else {
+                    RelayAssignment {
+                        relay: DbRelay::new(url.to_owned()),
+                        pubkeys: vec![],
+                    }
+                }
+            })
+            .collect();
 
         ui.push_id("general_feed_relays", |ui| {
             TableBuilder::new(ui)
@@ -56,16 +62,16 @@ pub(super) fn update(app: &mut GossipUi, ctx: &Context, frame: &mut eframe::Fram
                         ui.heading("Relay URL");
                     });
                     header.col(|ui| {
-                        ui.heading("Num Pubkeys");
+                        ui.heading("Num Keys");
                     });
                 })
                 .body(|body| {
-                    body.rows(24.0, relay_assignments.len(), |row_index, mut row| {
+                    body.rows(24.0, relays.len(), |row_index, mut row| {
                         row.col(|ui| {
-                            ui.label(&relay_assignments[row_index].relay.url.0);
+                            ui.label(&relays[row_index].relay.url.0);
                         });
                         row.col(|ui| {
-                            ui.label(format!("{}", relay_assignments[row_index].pubkeys.len()));
+                            ui.label(format!("{}", &relays[row_index].pubkeys.len()));
                         });
                     });
                 });
@@ -76,27 +82,32 @@ pub(super) fn update(app: &mut GossipUi, ctx: &Context, frame: &mut eframe::Fram
         ui.add_space(10.0);
 
         ui.heading("Coverage");
+        ui.add_space(12.0);
 
-        if !GLOBALS
-            .relay_picker
-            .blocking_read()
-            .pubkey_counts
-            .is_empty()
-        {
-            for (pk, count) in GLOBALS.relay_picker.blocking_read().pubkey_counts.iter() {
-                let maybe_person = GLOBALS.people.get(pk);
-                let name = match maybe_person {
-                    None => GossipUi::hex_pubkey_short(pk),
-                    Some(p) => match p.name() {
-                        None => GossipUi::hex_pubkey_short(pk),
-                        Some(n) => n.to_owned(),
-                    },
-                };
-                ui.label(format!("{}: coverage short by {} relay(s)", name, count));
-            }
-        } else {
-            ui.label("All followed people are fully covered.".to_owned());
-        }
+        ScrollArea::vertical()
+            .id_source("relay_coverage")
+            .show(ui, |ui| {
+                if !GLOBALS
+                    .relay_picker
+                    .blocking_read()
+                    .pubkey_counts
+                    .is_empty()
+                {
+                    for (pk, count) in GLOBALS.relay_picker.blocking_read().pubkey_counts.iter() {
+                        let maybe_person = GLOBALS.people.get(pk);
+                        let name = match maybe_person {
+                            None => GossipUi::hex_pubkey_short(pk),
+                            Some(p) => match p.name() {
+                                None => GossipUi::hex_pubkey_short(pk),
+                                Some(n) => n.to_owned(),
+                            },
+                        };
+                        ui.label(format!("{}: coverage short by {} relay(s)", name, count));
+                    }
+                } else {
+                    ui.label("All followed people are fully covered.".to_owned());
+                }
+            });
     } else if app.page == Page::RelaysAll {
         all::update(app, ctx, frame, ui);
     }
