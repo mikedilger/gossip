79b6b933ea4421cfbe1d6b06e9e536080ef8cdb4
Mike Dilger <mike@mikedilger.com>
2023-02-05 20:20:41 +1300

    improve prune.sql

--- src/db/prune.sql
+++ src/db/prune.sql
@@ -1,14 +1,14 @@
 
 PRAGMA foreign_keys = ON;
 
--- Delete stale kind=0 events (keep the last one)
-DELETE FROM event WHERE kind=0 AND created_at != (SELECT max(created_at) FROM event as event_inner WHERE kind=0 AND pubkey=event.pubkey);
+-- Delete stale kind=0,3 events (keep the last one)
+DELETE FROM event WHERE kind IN (0,3) AND created_at != (SELECT max(created_at) FROM event as event_inner WHERE kind=event.kind AND pubkey=event.pubkey);
 
--- Delete stale kind=3 events (keep the last one)
-DELETE FROM event WHERE kind=3 AND created_at != (SELECT max(created_at) FROM event as event_inner WHERE kind=3 AND pubkey=event.pubkey);
+-- Delete overridden replaceable events
+DELETE FROM event WHERE kind>=10000 and kind<20000 AND created_at != (SELECT max(created_at) FROM event as event_inner WHERE kind=event.kind AND pubkey=event.pubkey);
 
--- Delete stale kind=10001 events (keep the last one)
-DELETE FROM event WHERE kind=10001 AND created_at != (SELECT max(created_at) FROM event as event_inner WHERE kind=10001 AND pubkey=event.pubkey);
+-- Delete all ephemeral events
+DELETE FROM event WHERE kind>=20000 and kind<30000;
 
 -- Delete kind=1 events older than 1 week
 DELETE FROM event WHERE kind!=0 AND kind!=3 AND kind!=10001 AND created_at < strftime('%s', 'now') - 60*60*24*7;
