214eca37090fd493b9900808cf50bb2b2ada8238
Mike Dilger <mike@mikedilger.com>
2023-02-12 13:35:58 +1300

    0.3.93-unstable
    We will use odd point-versions with "-unstable" suffix for between-release commits
    and event point-versions for releases

--- packaging/tar/make-tar.sh
+++ packaging/tar/make-tar.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd ../../..
-tar -cv --exclude=gossip/.git --exclude=gossip/target --exclude=gossip/packaging -f - gossip | bzip2 -c > gossip/packaging/tar/gossip-0.3.91.tar.bz2
+tar -cv --exclude=gossip/.git --exclude=gossip/target --exclude=gossip/packaging -f - gossip | bzip2 -c > gossip/packaging/tar/gossip-0.3.93-unstable.tar.bz2
