653d3ba9d19061d2ba31516a709d82926f60995a
Mike Dilger <mike@ezicheq.com>
2023-02-15 15:19:12 +1300

    update make-tar.sh to use the current VERSION from Cargo.toml

--- packaging/tar/make-tar.sh
+++ packaging/tar/make-tar.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+VERSION=$(grep '^version' ../../Cargo.toml | awk -F\" '{print $2}')
+echo $VERSION;
+
 cd ../../..
-tar -cv --exclude=gossip/.git --exclude=gossip/target --exclude=gossip/packaging -f - gossip | bzip2 -c > gossip/packaging/tar/gossip-0.3.93-unstable.tar.bz2
+tar -cv --exclude=gossip/.git --exclude=gossip/target --exclude=gossip/packaging -f - gossip | bzip2 -c > gossip/packaging/tar/gossip-${VERSION}.tar.bz2
