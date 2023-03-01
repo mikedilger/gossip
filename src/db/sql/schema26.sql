3b61675556c8ac4e2c265cff3355e02c14f73e1d
Mike Dilger <mike@mikedilger.com>
2023-03-01 19:04:03 +1300

    Fix schema 26

--- src/db/sql/schema26.sql
+++ src/db/sql/schema26.sql
@@ -1,2 +1,2 @@
-insert into settings (key, value) values ('dark_mode', 1 - (SELECT value FROM settings WHERE key='light_mode'));
+insert into settings (key, value) values ('dark_mode', 1 - coalesce((SELECT value FROM settings WHERE key='light_mode'), 1));
 delete from settings where key = 'light_mode';
