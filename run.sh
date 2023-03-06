0a07985c7a27e5bb7929685369254856a24155be
Mike Dilger <mike@ezicheq.com>
2023-03-06 15:14:03 +1300

    Fix run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-RUSTFLAGS="-C target-cpu=native --cfg tokio_unstable" cargo build --release
-RUST_BACKTRACE=1 RUST_LOG="info,gossip=debug" ./target/release/gossip
+RUSTFLAGS="-C target-cpu=native --cfg tokio_unstable" cargo build --release && \
+    RUST_BACKTRACE=1 RUST_LOG="info,gossip=debug" ./target/release/gossip
 
