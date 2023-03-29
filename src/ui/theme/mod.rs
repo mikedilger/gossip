bf631c6b3fc9208bc16bed7cae7472580917acb3
Mike Dilger <mike@mikedilger.com>
2023-03-30 12:09:10 +1300

    Keep up to date with upstream egui

--- src/ui/theme/mod.rs
+++ src/ui/theme/mod.rs
@@ -347,6 +347,7 @@ pub(super) fn font_definitions() -> FontDefinitions {
                 scale: 1.22,            // This font is smaller than DejaVuSans
                 y_offset_factor: -0.18, // and too low
                 y_offset: 0.0,
+                baseline_offset_factor: 0.0,
             },
         ),
     );
@@ -357,8 +358,9 @@ pub(super) fn font_definitions() -> FontDefinitions {
         FontData::from_static(include_bytes!("../../../fonts/NotoEmoji-Regular.ttf")).tweak(
             FontTweak {
                 scale: 1.1,             // make them a touch larger
-                y_offset_factor: -0.26, // move them up
+                y_offset_factor: 0.0,
                 y_offset: 0.0,
+                baseline_offset_factor: 0.0,
             },
         ),
     );
