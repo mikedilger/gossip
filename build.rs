fc0badb3f3f3957f78105f1f9306362e35d064d8
Mike Dilger <mike@ezicheq.com>
2023-05-08 08:20:02 +1200

    build.rs link paths

--- build.rs
+++ build.rs
@@ -6,4 +6,11 @@ fn main() {
         .unwrap();
     let git_hash = String::from_utf8(output.stdout).unwrap();
     println!("cargo:rustc-env=GIT_HASH={git_hash}");
+
+    // link to bundled libraries
+    #[cfg(target_os="macos")]
+    println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path");
+
+    #[cfg(target_os="linux")]
+    println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
 }
