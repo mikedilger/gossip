972c4d4506c67465f49d431ec188502ec17b0b35
Mike Dilger <mike@mikedilger.com>
2023-07-09 00:47:18 +1200

    lmdb: Setup LMDB when creating Storage

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -1,9 +1,30 @@
 use crate::error::Error;
+use crate::profile::Profile;
+use lmdb::{Environment, EnvironmentFlags};
 
-pub struct Storage {}
+pub struct Storage {
+    env: Environment,
+}
 
 impl Storage {
     pub fn new() -> Result<Storage, Error> {
-        Ok(Storage {})
+        let mut builder = Environment::new();
+
+        builder.set_flags(
+            EnvironmentFlags::WRITE_MAP | // no nested transactions!
+            EnvironmentFlags::NO_META_SYNC |
+            EnvironmentFlags::MAP_ASYNC,
+        );
+        // builder.set_max_readers(126); // this is the default
+        builder.set_max_dbs(32);
+
+        // This has to be big enough for all the data.
+        // Note that it is the size of the map in VIRTUAL address space,
+        //   and that it doesn't all have to be paged in at the same time.
+        builder.set_map_size(1048576 * 1024 * 2); // 2 GB (probably too small)
+
+        let env = builder.open(&Profile::current()?.lmdb_dir)?;
+
+        Ok(Storage { env })
     }
 }
