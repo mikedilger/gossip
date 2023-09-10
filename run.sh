d4a90cd56a98cac8851571e72078487225ceb9f1
Mike Dilger <mike@mikedilger.com>
2023-09-10 14:45:15 +1200

    update nostr-types

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 #RUSTFLAGS="-C target-cpu=native --cfg tokio_unstable"
 cargo build --features=lang-cjk,video-ffmpeg --release && \
-    RUST_LOG="info" ./target/release/gossip login
+    RUST_BACKTRACE=1 RUST_LOG="info" ./target/release/gossip login
 
