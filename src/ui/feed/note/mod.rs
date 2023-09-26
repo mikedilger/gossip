f2f10005ea079b1694705454ad2c9182b610861c
Mike Dilger <mike@mikedilger.com>
2023-09-27 09:59:21 +1300

    Do not mute notes when looking at the muted person's feed itself

--- src/ui/feed/note/mod.rs
+++ src/ui/feed/note/mod.rs
@@ -68,7 +68,9 @@ pub(super) fn render_note(
         // FIXME drop the cached notes on recompute
 
         if let Ok(note_data) = note_ref.try_borrow() {
-            let skip = (note_data.muted() && !matches!(app.page, Page::Feed(FeedKind::DmChat(_))))
+            let skip = (note_data.muted()
+                && !matches!(app.page, Page::Feed(FeedKind::DmChat(_)))
+                && !matches!(app.page, Page::Feed(FeedKind::Person(_))))
                 || (note_data.deletion.is_some() && !app.settings.show_deleted_events);
 
             if skip {
