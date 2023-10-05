0a18e72125a25123abd2d03a5c81c5b522cb18cd
Nicola Benaglia <nico.benaz@gmail.com>
2023-10-05 09:01:21 +0200

    Remove files

--- filescontaining.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-find . -name .git -prune -o -name target -prune -o -type f -exec grep -H "$1" {} \; \
-     | awk -F: '{print $1}' | uniq
--- find.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-find . -name .git -prune -o -name target -prune -o -type f -exec grep -H "$1" {} \;
