e9a72471c78d62cbdf60b0dfe51d03d6e7b59590
Mike Dilger <mike@mikedilger.com>
2023-10-22 17:53:25 +1300

    improve PersonList type a bit

--- gossip-lib/src/storage/types/person_list1.rs
+++ gossip-lib/src/storage/types/person_list1.rs
@@ -1,6 +1,7 @@
 use crate::error::{Error, ErrorKind};
 use crate::globals::GLOBALS;
 use heed::RwTxn;
+use nostr_types::EventKind;
 use speedy::{Readable, Writable};
 
 /// Lists people can be added to
@@ -51,14 +52,34 @@ impl PersonList1 {
         let mut output: Vec<(PersonList1, String)> = vec![];
         let map = GLOBALS.storage.read_setting_custom_person_list_map();
         for (k, v) in map.iter() {
-            output.push((PersonList1::Custom(*k), v.clone()));
+            match k {
+                0 => output.push((PersonList1::Muted, v.clone())),
+                1 => output.push((PersonList1::Followed, v.clone())),
+                _ => output.push((PersonList1::Custom(*k), v.clone())),
+            }
         }
         output
     }
 
     /// Allocate a new PersonList1 with the given name
     pub fn allocate(name: &str, txn: Option<&mut RwTxn<'_>>) -> Result<PersonList1, Error> {
+        // Do not allocate for well-known names
+        if name == "Followed" {
+            return Ok(PersonList1::Followed);
+        } else if name == "Muted" {
+            return Ok(PersonList1::Muted);
+        }
+
         let mut map = GLOBALS.storage.read_setting_custom_person_list_map();
+
+        // Check if it already exists to prevent duplicates
+        for (k, v) in map.iter() {
+            if v == name {
+                return Ok(PersonList1::Custom(*k));
+            }
+        }
+
+        // Find a slot and allocate
         for i in 2..255 {
             if map.contains_key(&i) {
                 continue;
@@ -69,6 +90,7 @@ impl PersonList1 {
                 .write_setting_custom_person_list_map(&map, txn)?;
             return Ok(PersonList1::Custom(i));
         }
+
         Err(ErrorKind::NoSlotsRemaining.into())
     }
 
@@ -102,6 +124,15 @@ impl PersonList1 {
                     None => "Unallocated".to_owned(),
                 }
             }
+        }
+    }
+
+    /// Get the event kind matching this PersonList1
+    pub fn event_kind(&self) -> EventKind {
+        match *self {
+            PersonList1::Followed => EventKind::ContactList,
+            PersonList1::Muted => EventKind::MuteList,
+            PersonList1::Custom(_) => EventKind::CategorizedPeopleList,
         }
     }
 
