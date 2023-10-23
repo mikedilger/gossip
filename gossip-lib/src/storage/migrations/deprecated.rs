1ff2f1dad9841fa05bf49a01eb9c2c4b2753367e
Mike Dilger <mike@mikedilger.com>
2023-10-24 08:45:46 +1300

    Fix a hang during migration 14 for new users

--- gossip-lib/src/storage/migrations/deprecated.rs
+++ gossip-lib/src/storage/migrations/deprecated.rs
@@ -39,38 +39,10 @@ impl Storage {
         match self.general.get(&txn, b"last_contact_list_edit")? {
             None => {
                 let now = Unixtime::now().unwrap();
-                self.write_last_contact_list_edit(now.0, None)?;
                 Ok(now.0)
             }
             Some(bytes) => Ok(i64::from_be_bytes(bytes[..8].try_into().unwrap())),
         }
-    }
-
-    /// Write the user's last MuteList edit time
-    /// DEPRECATED - use set_person_list_last_edit_time instead
-    pub(in crate::storage) fn write_last_mute_list_edit<'a>(
-        &'a self,
-        when: i64,
-        rw_txn: Option<&mut RwTxn<'a>>,
-    ) -> Result<(), Error> {
-        let bytes = when.to_be_bytes();
-
-        let f = |txn: &mut RwTxn<'a>| -> Result<(), Error> {
-            self.general
-                .put(txn, b"last_mute_list_edit", bytes.as_slice())?;
-            Ok(())
-        };
-
-        match rw_txn {
-            Some(txn) => f(txn)?,
-            None => {
-                let mut txn = self.env.write_txn()?;
-                f(&mut txn)?;
-                txn.commit()?;
-            }
-        };
-
-        Ok(())
     }
 
     /// Read the user's last MuteList edit time
@@ -81,7 +53,6 @@ impl Storage {
         match self.general.get(&txn, b"last_mute_list_edit")? {
             None => {
                 let now = Unixtime::now().unwrap();
-                self.write_last_mute_list_edit(now.0, None)?;
                 Ok(now.0)
             }
             Some(bytes) => Ok(i64::from_be_bytes(bytes[..8].try_into().unwrap())),
