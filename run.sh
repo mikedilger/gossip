43ca24f7021be292fe804f5489e7b2d28c74ce96
Mike Dilger <mike@mikedilger.com>
2023-11-27 20:25:22 +1300

    fix subtle bug in run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 #RUSTFLAGS="-C target-cpu=native --cfg tokio_unstable"
 cargo build --features=lang-cjk,video-ffmpeg --release && \
-    RUST_BACKTRACE=1 RUST_LOG="info" ./target/release/gossip $@
+    RUST_BACKTRACE=1 RUST_LOG="info" ./target/release/gossip "$@"
 
