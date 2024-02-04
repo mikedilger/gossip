59787b0ab88e9d390bd908ffc32d9dff65d3bf1a
Mike Dilger <mike@mikedilger.com>
2024-02-05 09:22:09 +1300

    run.sh: don't include ffmpeg (crashes too often for me)

--- debug.sh
+++ debug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cargo build --features=lang-cjk,video-ffmpeg --release && \
+cargo build --features=lang-cjk --release && \
     RUST_BACKTRACE=1 RUST_LOG="info,gossip_lib=debug" ./target/release/gossip "$@" \
         | tee gossip.log.txt
 
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 #RUSTFLAGS="-C target-cpu=native --cfg tokio_unstable"
-cargo build --features=lang-cjk,video-ffmpeg --release && \
+cargo build --features=lang-cjk --release && \
     RUST_BACKTRACE=1 RUST_LOG="warn,gossip_bin=info,gossip_lib=info,nostr_types=info,gossip_relay_picker=info" ./target/release/gossip "$@"
 
