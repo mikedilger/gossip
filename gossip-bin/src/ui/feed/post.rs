45eb3648fe5f4c1e81897da7170b2fb3627ef092
Bu5hm4nn <bu5hm4nn@users.noreply.github.com>
2024-03-26 14:52:01 -0600

    Tagging: Prioritize not only followed people but people on all lists

--- gossip-bin/src/ui/feed/post.rs
+++ gossip-bin/src/ui/feed/post.rs
@@ -673,14 +673,8 @@ fn calc_tagging_search(app: &mut GossipUi) {
                 .search_people_to_tag(search)
                 .unwrap_or_default();
             pairs.sort_by(|(_, ak), (_, bk)| {
-                let af = GLOBALS
-                    .storage
-                    .is_person_in_list(ak, gossip_lib::PersonList::Followed)
-                    .unwrap_or(false);
-                let bf = GLOBALS
-                    .storage
-                    .is_person_in_list(bk, gossip_lib::PersonList::Followed)
-                    .unwrap_or(false);
+                let af = GLOBALS.storage.is_person_subscribed_to(ak).unwrap_or(false);
+                let bf = GLOBALS.storage.is_person_subscribed_to(bk).unwrap_or(false);
                 bf.cmp(&af).then(std::cmp::Ordering::Greater)
             });
             app.draft_data.tagging_search_searched = Some(search.clone());
