368e47cb36c62e8df32dd14b00d2ae37462da7dd
Mike Dilger <mike@mikedilger.com>
2024-04-03 11:29:49 +1300

    fix run.sh (it was building debug, running release!)

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 #RUSTFLAGS="-C target-cpu=native --cfg tokio_unstable"
 cargo build --features=lang-cjk,video-ffmpeg && \
-    RUST_BACKTRACE=1 RUST_LOG="warn,gossip=info,gossip_lib=info,nostr_types=info,gossip_relay_picker=info" ./target/release/gossip "$@"
+    RUST_BACKTRACE=1 RUST_LOG="warn,gossip=info,gossip_lib=info,nostr_types=info,gossip_relay_picker=info" ./target/debug/gossip "$@"
