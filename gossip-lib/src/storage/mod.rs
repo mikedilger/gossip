ed3eb318b944d624c729cfe9ee89943ab7143a46
Mike Dilger <mike@mikedilger.com>
2024-08-08 08:32:09 +1200

    optimize: avoid a copy of found events

--- gossip-lib/src/storage/mod.rs
+++ gossip-lib/src/storage/mod.rs
@@ -1603,10 +1603,9 @@ impl Storage {
         }
 
         Ok(output
-            .iter()
+            .into_iter()
             .rev()
             .take(limit)
-            .cloned() // FIXME when BTreeSet gets a drain() function
             .collect())
     }
 
