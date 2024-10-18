32a7527b2aeb1762068e065d59443a0054cea651
Bu5hm4nn <bu5hm4nn@users.noreply.github.com>
2024-10-18 16:46:29 -0400

    Handler UI: Update for design

--- gossip-bin/src/ui/handler.rs
+++ gossip-bin/src/ui/handler.rs
@@ -1,16 +1,12 @@
-use core::f32;
-
 use super::{widgets, GossipUi};
-use eframe::egui::{self, Widget, WidgetText};
+use eframe::egui::{self};
 use egui::{Context, Ui};
 use gossip_lib::comms::ToOverlordMessage;
 use gossip_lib::{HandlerKey, HandlersTable, Table, GLOBALS};
 use nostr_types::{EventKind, NAddr};
 
-pub(super) fn update_all_kinds(app: &mut GossipUi, _ctx: &Context, ui: &mut Ui) {
+pub(super) fn update_all_kinds(app: &mut GossipUi, ctx: &Context, ui: &mut Ui) {
     widgets::page_header(ui, "External Event Handlers", |_ui| ());
-
-    let panel_height = ui.available_height();
 
     ui.label("Import a handler via nevent");
     let response = ui.add(text_edit_line!(app, app.handler_naddr).hint_text("naddr1..."));
@@ -51,111 +47,155 @@ pub(super) fn update_all_kinds(app: &mut GossipUi, _ctx: &Context, ui: &mut Ui) 
         kinds.dedup();
 
         for kind in kinds.iter() {
-            widgets::list_entry::clickable_frame(
+            let response = widgets::list_entry::clickable_frame(
                 ui,
                 app,
                 Some(app.theme.main_content_bgcolor()),
                 |ui, app| {
                     ui.set_min_width(ui.available_width());
-                    ui.set_height(37.0);
 
-                    let kind_name = format!("{}", kind);
-                    let id = egui::Id::new(u32::from(*kind).to_string());
+                    let kind_name = format!("Kind {:?}", u32::from(*kind));
+
+                    let handlers: Vec<(HandlerKey, bool, bool)> = GLOBALS
+                        .db()
+                        .read_configured_handlers(*kind)
+                        .unwrap_or(vec![]);
+
+                    let all_count = handlers.len();
+                    let enabled_count = handlers.iter().filter_map(|f| f.1.then(|| {})).count();
+                    let recommended_count = handlers.iter().filter_map(|f| f.2.then(|| {})).count();
 
                     ui.horizontal(|ui| {
-                        ui.label(egui::RichText::new(&kind_name).heading());
-                        ui.add_space(10.0);
-                        ui.label(egui::RichText::new(u32::from(*kind).to_string()).heading());
-                    });
+                        let kwidth = ui.label(egui::RichText::new(&kind_name)).rect.width();
+                        ui.add_space(200.0 - kwidth);
+                        ui.label(egui::RichText::new(format!("{:?}", kind)));
 
-                    egui::CollapsingHeader::new("Configure Handlers")
-                        .id_source(id)
-                        .show_unindented(ui, |ui| {
-                            let handlers: Vec<(HandlerKey, bool, bool)> = GLOBALS
-                                .db()
-                                .read_configured_handlers(*kind)
-                                .unwrap_or(vec![]);
-
-                            if ui.button("Share recommendations").clicked() {
-                                let _ = GLOBALS
-                                    .to_overlord
-                                    .send(ToOverlordMessage::ShareHandlerRecommendations(*kind));
+                        ui.with_layout(egui::Layout::right_to_left(egui::Align::default()), |ui| {
+                            if all_count == 0 {
+                                ui.label(egui::RichText::new("No recommendations").weak());
+                            } else {
+                                if recommended_count != all_count {
+                                    ui.label(
+                                        egui::RichText::new(format!(
+                                            "{} of {} apps",
+                                            enabled_count, all_count
+                                        ))
+                                        .color(app.theme.accent_color()),
+                                    );
+                                } else {
+                                    ui.label(
+                                        egui::RichText::new(format!("{} apps", all_count))
+                                            .color(app.theme.accent_color()),
+                                    );
+                                }
                             }
-
-                            app.vert_scroll_area()
-                                .id_source(id.with("s"))
-                                .max_width(f32::INFINITY)
-                                .max_height(panel_height * 0.66)
-                                .show(ui, |ui| {
-                                    for (key, mut enabled, mut recommended) in handlers.iter() {
-                                        if let Ok(Some(handler)) =
-                                            HandlersTable::read_record(key.clone(), None)
-                                        {
-                                            if !kind.is_parameterized_replaceable()
-                                                && handler.nevent_url.is_none()
-                                            {
-                                                continue;
-                                            }
-                                            if kind.is_parameterized_replaceable()
-                                                && handler.naddr_url.is_none()
-                                            {
-                                                continue;
-                                            }
-                                            let name = match handler.bestname(*kind) {
-                                                Some(n) => n,
-                                                None => continue,
-                                            };
-
-                                            ui.with_layout(
-                                                egui::Layout::left_to_right(egui::Align::TOP),
-                                                |ui| {
-                                                    ui.label(name);
-                                                    if widgets::Switch::small(
-                                                        &app.theme,
-                                                        &mut enabled,
-                                                    )
-                                                    .with_label("enable")
-                                                    .show(ui)
-                                                    .changed()
-                                                    {
-                                                        let _ =
-                                                            GLOBALS.db().write_configured_handler(
-                                                                *kind,
-                                                                key.clone(),
-                                                                enabled,
-                                                                recommended,
-                                                                None,
-                                                            );
-                                                    }
-                                                    if widgets::Switch::small(
-                                                        &app.theme,
-                                                        &mut recommended,
-                                                    )
-                                                    .with_label("recommend")
-                                                    .show(ui)
-                                                    .changed()
-                                                    {
-                                                        let _ =
-                                                            GLOBALS.db().write_configured_handler(
-                                                                *kind,
-                                                                key.clone(),
-                                                                enabled,
-                                                                recommended,
-                                                                None,
-                                                            );
-                                                    }
-                                                },
-                                            );
-
-                                            ui.add_space(7.0);
-                                        }
-                                    }
-                                });
                         });
+                    })
                 },
             );
+
+            if response
+                .response
+                .on_hover_cursor(egui::CursorIcon::PointingHand)
+                .interact(egui::Sense::click())
+                .clicked()
+            {
+                app.set_page(ctx, super::Page::Handlers(*kind));
+            }
         }
     });
 }
 
-pub(super) fn update_kind(_app: &mut GossipUi, _ctx: &Context, ui: &mut Ui, kind: EventKind) {}
+pub(super) fn update_kind(app: &mut GossipUi, _ctx: &Context, ui: &mut Ui, kind: EventKind) {
+    widgets::page_header(
+        ui,
+        format!("Handler: {:?} ({})", kind, u32::from(kind)),
+        |ui| {
+            if widgets::Button::secondary(&app.theme, "Share recommendations")
+                .show(ui)
+                .clicked()
+            {
+                let _ = GLOBALS
+                    .to_overlord
+                    .send(ToOverlordMessage::ShareHandlerRecommendations(kind));
+            }
+        },
+    );
+
+    let handlers: Vec<(HandlerKey, bool, bool)> = GLOBALS
+        .db()
+        .read_configured_handlers(kind)
+        .unwrap_or(vec![]);
+
+    app.vert_scroll_area().show(ui, |ui| {
+        for (key, mut enabled, mut recommended) in handlers.iter() {
+            if let Ok(Some(handler)) = HandlersTable::read_record(key.clone(), None) {
+                if !kind.is_parameterized_replaceable() && handler.nevent_url.is_none() {
+                    continue;
+                }
+                if kind.is_parameterized_replaceable() && handler.naddr_url.is_none() {
+                    continue;
+                }
+
+                let name = match handler.bestname(kind) {
+                    Some(n) => n,
+                    None => continue,
+                };
+
+                widgets::list_entry::clickable_frame(
+                    ui,
+                    app,
+                    Some(app.theme.main_content_bgcolor()),
+                    |ui, app| {
+                        ui.set_min_width(ui.available_width());
+
+                        ui.horizontal(|ui| {
+                            if widgets::Switch::small(&app.theme, &mut enabled)
+                                .show(ui)
+                                .changed()
+                            {
+                                let _ = GLOBALS.db().write_configured_handler(
+                                    kind,
+                                    key.clone(),
+                                    enabled,
+                                    recommended,
+                                    None,
+                                );
+                            }
+
+                            ui.add_space(10.0);
+                            let lwidth = ui.label(&name).rect.width();
+
+                            ui.add_space(200.0 - lwidth);
+
+                            if widgets::Switch::small(&app.theme, &mut recommended)
+                                .with_label("recommend")
+                                .show(ui)
+                                .changed()
+                            {
+                                let _ = GLOBALS.db().write_configured_handler(
+                                    kind,
+                                    key.clone(),
+                                    enabled,
+                                    recommended,
+                                    None,
+                                );
+                            }
+                        });
+
+                        ui.horizontal(|ui| {
+                            ui.label("About: ");
+                            if let Some(metadata) = handler.metadata() {
+                                ui.label(metadata.about.as_deref().unwrap_or("".into()));
+                            }
+                        });
+
+                        ui.horizontal(|ui| {
+                            ui.label("Recommended by: <TODO>");
+                        });
+                    },
+                );
+            }
+        }
+    });
+}
