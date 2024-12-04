d7401267ace2056469a8e4d03a69aa21a33cf7c3
Mike Dilger <mike@mikedilger.com>
2024-12-05 10:19:54 +1300

    Use SortablePubkey and BTreeSet in Followers so we can keep them sorted by name

--- gossip-lib/src/followers.rs
+++ gossip-lib/src/followers.rs
@@ -1,16 +1,17 @@
 use nostr_types::PublicKey;
-use std::collections::HashSet;
+use std::collections::BTreeSet;
+use crate::people::SortablePubkey;
 
 pub struct Followers {
     pub who: Option<PublicKey>,
-    pub set: HashSet<PublicKey>,
+    pub set: BTreeSet<SortablePubkey>,
 }
 
 impl Default for Followers {
     fn default() -> Followers {
         Followers {
             who: None,
-            set: HashSet::new(),
+            set: BTreeSet::new(),
         }
     }
 }
@@ -18,14 +19,14 @@ impl Default for Followers {
 impl Followers {
     pub fn reset(&mut self, pubkey: PublicKey) {
         self.who = Some(pubkey);
-        self.set = HashSet::new();
+        self.set = BTreeSet::new();
     }
 
     pub fn add_follower(&mut self, follower: PublicKey) {
-        self.set.insert(follower);
+        self.set.insert(follower.into());
     }
 
-    pub fn get(&mut self) -> Vec<PublicKey> {
-        self.set.iter().map(|&k| k).collect()
+    pub fn get_range(&mut self, start: usize, amount: usize) -> Vec<PublicKey> {
+        self.set.iter().skip(start).take(amount).map(|k| (*k).into()).collect()
     }
 }
