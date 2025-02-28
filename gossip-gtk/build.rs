86a6b51d447384e92e477a0c8db0ee4b53af466a
Mike Dilger <mike@mikedilger.com>
2025-03-01 12:02:42 +1300

    gtk: build.rs, logging, about

---
+++ gossip-gtk/build.rs
@@ -0,0 +1,17 @@
+use std::process::Command;
+
+fn main() {
+    // link to bundled libraries
+    #[cfg(target_os = "macos")]
+    println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path");
+
+    #[cfg(target_os = "linux")]
+    println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
+
+    let output = Command::new("git")
+        .args(["rev-parse", "HEAD"])
+        .output()
+        .unwrap();
+    let git_hash = String::from_utf8(output.stdout).unwrap();
+    println!("cargo:rustc-env=GIT_HASH={git_hash}");
+}
--- gossip-gtk/src/main.rs
+++ gossip-gtk/src/main.rs
@@ -1,4 +1,46 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "console")]
+#![allow(clippy::collapsible_if)]
+#![allow(clippy::collapsible_else_if)]
+// TEMPORARILY
+#![allow(clippy::uninlined_format_args)]
+#![allow(clippy::assigning_clones)]
 
-fn main() {
-    println!("Hello World");
+mod about;
+
+use gossip_lib::Error;
+use std::env;
+use tracing_subscriber::filter::{EnvFilter, LevelFilter};
+
+fn main() -> Result<(), Error> {
+    setup_logging();
+
+    let about = about::About::new();
+    tracing::info!("Gossip-GTK {}", about.version);
+
+    Ok(())
+}
+
+
+fn setup_logging() {
+    // Setup logging
+    let env_filter = if env::var("RUST_LOG").is_err() {
+        EnvFilter::builder()
+            .with_default_directive(LevelFilter::INFO.into())
+            .from_env_lossy()
+    } else {
+        EnvFilter::from_default_env()
+    };
+
+    let max_level = match env_filter.max_level_hint() {
+        Some(l) => l,
+        None => LevelFilter::ERROR,
+    };
+    let show_debug = cfg!(debug_assertions) || max_level <= LevelFilter::DEBUG;
+    tracing_subscriber::fmt::fmt()
+        .with_writer(std::io::stderr)
+        .with_target(false)
+        .with_file(show_debug)
+        .with_line_number(show_debug)
+        .with_env_filter(env_filter)
+        .init();
 }
