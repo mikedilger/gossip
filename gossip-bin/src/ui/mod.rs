e873e6dbc0495bd6fdd8708e9a1fc569aec10afa
Mike Dilger <mike@mikedilger.com>
2025-04-08 13:31:11 +1200

    Toasts start

--- gossip-bin/src/ui/mod.rs
+++ gossip-bin/src/ui/mod.rs
@@ -59,8 +59,8 @@ use eframe::egui::vec2;
 use eframe::egui::FontId;
 use egui::widgets::Slider;
 use egui::{
-    Align, Color32, ColorImage, Context, IconData, Image, ImageData, Label, Layout, RichText,
-    ScrollArea, Sense, TextureHandle, TextureOptions, Ui, Vec2,
+    Align, Color32, ColorImage, Context, IconData, Image, ImageData, Label, Layout,
+    Pos2, RichText, ScrollArea, Sense, TextureHandle, TextureOptions, Ui, Vec2,
 };
 use egui_file_dialog::FileDialog;
 #[cfg(feature = "video-ffmpeg")]
@@ -453,6 +453,10 @@ struct GossipUi {
     // Modal dialogue
     modal: Option<Rc<ModalEntry>>,
 
+    // Toasts
+    toasts: HashMap<usize, String>,
+    next_toast_num: usize,
+
     // QR codes being rendered (in feed or elsewhere)
     // the f32's are the recommended image size
     qr_codes: HashMap<String, Result<(TextureHandle, f32, f32), Error>>,
@@ -741,6 +745,10 @@ impl GossipUi {
             }
         });
 
+        let mut toasts: HashMap<usize, String> = HashMap::new();
+        toasts.insert(0, "This is an example toast".to_string());
+        toasts.insert(1, "This is a second example toast".to_string());
+
         GossipUi {
             #[cfg(feature = "video-ffmpeg")]
             audio_device,
@@ -757,6 +765,8 @@ impl GossipUi {
             frame_count: 0,
             popups: HashMap::new(),
             modal: None,
+            toasts,
+            next_toast_num: 2,
             qr_codes: HashMap::new(),
             notecache: NoteCache::new(),
             notification_data: NotificationData::new(),
@@ -2197,6 +2207,16 @@ impl GossipUi {
             GLOBALS.status_queue.write().dismiss(2);
         }
     }
+
+    fn add_toast(&mut self, s: String) {
+        let num = self.next_toast_num;
+        self.next_toast_num += 1;
+        self.toasts.insert(num, s);
+    }
+
+    fn rm_toast(&mut self, u: usize) {
+        self.toasts.remove(&u);
+    }
 }
 
 impl eframe::App for GossipUi {
@@ -2473,7 +2493,7 @@ impl eframe::App for GossipUi {
             ZapState::ReadyToPay(id, _) => Some(id),
         };
 
-        egui::CentralPanel::default()
+        let ir = egui::CentralPanel::default()
             .frame({
                 let frame = egui::Frame::central_panel(&self.theme.get_style());
                 frame.inner_margin(egui::Margin {
@@ -2514,6 +2534,32 @@ impl eframe::App for GossipUi {
                     Page::Wizard(_) => unreachable!(),
                 }
             });
+
+        let central_panel_response = ir.response;
+
+        // Render toasts
+        let toasts = self.toasts.clone();
+        let mut y = central_panel_response.rect.max.y - 100.0;
+        for (u, s) in &toasts {
+            let pos = Pos2 {
+                x: central_panel_response.rect.max.x - 100.0,
+                y,
+            };
+            let response = egui::containers::popup::show_tooltip_at(
+                ctx,
+                central_panel_response.layer_id,
+                egui::Id::new(u),
+                pos,
+                |ui| -> Response {
+                    let response = ui.add(Label::new(s).sense(Sense::click()));
+                    if response.clicked() {
+                        self.rm_toast(*u);
+                    };
+                    response
+                }
+            );
+            y = response.rect.min.y - 50.0;
+        }
     }
 }
 
