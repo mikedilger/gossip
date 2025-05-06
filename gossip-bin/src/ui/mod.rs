e94802c5e0315f2e954310e0e1807a81dd577012
Mike Dilger <mike@mikedilger.com>
2025-05-07 10:19:54 +1200

    PARTIAL DEBUGGING

--- gossip-bin/src/ui/mod.rs
+++ gossip-bin/src/ui/mod.rs
@@ -596,6 +596,7 @@ impl Drop for GossipUi {
         self.password.zeroize();
         self.password2.zeroize();
         self.password3.zeroize();
+        self.import_priv.zeroize();
     }
 }
 
--- gossip-bin/src/ui/wizard/setup_remote_signer.rs
+++ gossip-bin/src/ui/wizard/setup_remote_signer.rs
@@ -13,25 +13,25 @@ pub(super) fn update(app: &mut GossipUi, ctx: &Context, _frame: &mut eframe::Fra
 
     egui::Grid::new("signerurl")
         .num_columns(2)
-        .striped(false)
-        .spacing([10.0, 10.0])
-        .show(ui, |ui| {
-            ui.label("Enter the remote signer URL");
-            let response = text_edit_line!(app, app.wizard_state.remote_signer_url)
-                .password(false)
-                .with_paste()
-                .show(ui)
-                .response;
+         .striped(false)
+         .spacing([10.0, 10.0])
+         .show(ui, |ui| {
+             ui.label("Enter the remote signer URL");
+             // NOTE: there is a bug in our TextEdit widget that causes right-aligned
+             //       elements on the page to slide off the right side of the window
+             //       when the input content gets long.  So instead of using
+             //       our text_edit_line!, I am working around it with the upstream's.
+             let response = egui::TextEdit::singleline(&mut app.wizard_state.remote_signer_url)
+                 .show(ui)
+                 .response;
             if response.changed() {
                 app.wizard_state.error = None;
             }
             ui.end_row();
-
             ui.label(""); // empty cell
-        });
+         });
 
-    let password_mismatch = app.password != app.password2;
-    let ready = !app.wizard_state.remote_signer_url.is_empty() && !password_mismatch;
+    let ready = !app.wizard_state.remote_signer_url.is_empty() && (app.password == app.password2);
 
     ui.add_space(20.0);
     egui::Grid::new("inputs")
@@ -49,8 +49,8 @@ pub(super) fn update(app: &mut GossipUi, ctx: &Context, _frame: &mut eframe::Fra
                 app.wizard_state.error = None;
             }
             ui.end_row();
-
             ui.label("Repeat passphrase to be sure");
+
             let response = text_edit_line!(app, app.password2)
                 .password(true)
                 .with_paste()
@@ -60,8 +60,8 @@ pub(super) fn update(app: &mut GossipUi, ctx: &Context, _frame: &mut eframe::Fra
                 app.wizard_state.error = None;
             }
             ui.end_row();
-
             ui.label(""); // empty cell
+
             let text = if ready {
                 if app.password.is_empty() {
                     "Your password is empty!"
@@ -69,9 +69,14 @@ pub(super) fn update(app: &mut GossipUi, ctx: &Context, _frame: &mut eframe::Fra
                     ""
                 }
             } else {
-                "Passwords do not match."
+                if app.wizard_state.remote_signer_url.is_empty() {
+                    "Enter a URL"
+                } else {
+                    "Passwords do not match."
+                }
             };
             ui.label(RichText::new(text).color(app.theme.warning_marker_text_color()));
+            ui.label(""); // empty cell
             ui.end_row();
         });
 
@@ -84,7 +89,7 @@ pub(super) fn update(app: &mut GossipUi, ctx: &Context, _frame: &mut eframe::Fra
     wizard_controls(
         ui,
         app,
-        ready,
+        true, // FIXME
         |app| {
             app.set_page(ctx, Page::Wizard(WizardPage::WelcomeGossip));
         },
@@ -96,10 +101,13 @@ pub(super) fn update(app: &mut GossipUi, ctx: &Context, _frame: &mut eframe::Fra
                     app.wizard_state.remote_signer_url.clone(),
                     app.password.clone(),
                 ));
-            app.password.zeroize();
-            app.password = "".to_owned();
-            app.password2.zeroize();
-            app.password2 = "".to_owned();
+            // Leave this incase they need to try again, and to avoid the "password is empty"
+            // message.
+            // FIXME: where to clear/zeroize?
+            //app.password.zeroize();
+            //app.password = "".to_owned();
+            //app.password2.zeroize();
+            //app.password2 = "".to_owned();
         },
-    );
+    )
 }
