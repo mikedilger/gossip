b402bdde34d055f86b8bcb7ed8ee016c0731c679
yggverse <yggverse@project>
2026-06-20 01:38:46 +0000

    show link address on hover tooltip; unify case

--- gossip-bin/src/ui/widgets/link_context_menu.rs
+++ gossip-bin/src/ui/widgets/link_context_menu.rs
@@ -4,7 +4,10 @@ use gossip_lib::GLOBALS;
 use nostr_types::Url;
 
 fn draw_open_and_copy(ui: &mut Ui, url_string: String) {
-    if ui.button("Open in browser").clicked() {
+    if ui
+        .button(format!("Open in browser ({url_string})"))
+        .clicked()
+    {
         let modifiers = ui.ctx().input(|i| i.modifiers);
         ui.ctx().output_mut(|o| {
             o.commands
@@ -20,7 +23,7 @@ fn draw_open_and_copy(ui: &mut Ui, url_string: String) {
             .write("Opening in browser...".to_owned());
     }
 
-    if ui.button("Copy URL").clicked() {
+    if ui.button(format!("Copy URL ({url_string})")).clicked() {
         ui.output_mut(|o| {
             o.commands
                 .push(egui::OutputCommand::CopyText(url_string.clone()))
